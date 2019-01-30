80340c4ece5b54c4df012ccb6318157b857eaca4
liyukun0111 <905284688@qq.com>
2019-01-30 15:30:38 +0800

    2019-01-30

--- demo/src/main.js
+++ demo/src/main.js
@@ -1,9 +1,27 @@
+//  引入Vue
 import Vue from 'vue'
+
+// 引入 ElementUI的组件  
+import ElementUI from 'element-ui'
+// 和  样式
+import 'element-ui/lib/theme-chalk/index.css'
+
+//  引入顶级组件 App.vue
 import App from './App.vue'
+
+//  引入路由
 import router from './router'
 
+//引入公共样式
+import '@/styles/common.less'
+
+//  使用elementui
+Vue.use(ElementUI)
+
+//  阻止生产提示
 Vue.config.productionTip = false
 
+// 创建vue实例对象 挂载dom 把路由 和 app顶级组件传入 渲染进入dom容器中
 new Vue({
   router,
   render: h => h(App)
---
+++ demo/src/views/Index/Index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="index">
+        主页
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
